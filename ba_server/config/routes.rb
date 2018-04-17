6f020ecf6e1e23eb2d28e2233173b8fe6eabe63d
sonia08 <soniabansal@Sonias-MacBook-Pro.local>
2018-04-17 17:42:54 +1000

    routes

--- ba_server/config/routes.rb
+++ ba_server/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root :to =>'airplanes#home'
   resources :users
-  resources :reservations
   resources :flights
   resources :airplanes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
+  # resources :reservations
