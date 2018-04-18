ddd90f3bbd800c71e31c89799365c8a33e22e686
Julie Pettersson <juliepettersson@Julies-MacBook-Air.local>
2018-04-18 11:14:58 +1000

    updated seed file

--- ba_server/db/seeds.rb
+++ ba_server/db/seeds.rb
@@ -8,7 +8,7 @@ a1 = Airplane.create(:name => 'B700', :rows => '4', :columns => '10', :total_sea
 a2 = Airplane.create(:name => 'B710', :rows => '4', :columns => '12', :total_seats => '48')
 a3 = Airplane.create(:name => 'B720', :rows => '4', :columns => '14', :total_seats => '56')
 
-Flights.destroy_all
+Flight.destroy_all
 f1 = Flight.create(:flight_no => '25', :origin => 'Sydney', :destination => 'Brisbane', :date => '2018-07-22', :avail_seats => '20')
 f2 = Flight.create(:flight_no => '30', :origin => 'Perth', :destination => 'Darwin', :date => '2018-05-19', :avail_seats => '34')
 f3 = Flight.create(:flight_no => '35', :origin => 'Melborne', :destination => 'Cairns', :date => '2018-06-02', :avail_seats => '4')
